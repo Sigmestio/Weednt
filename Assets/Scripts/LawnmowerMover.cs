a82556766c58a2b3269a030dfa54fdb808930a2f
Sigma <jskolska@edu.cdv.pl>
2024-04-09 23:29:07 +0200

    placeholders
    level on placeholders, scripts are working, its awesome I KNOW IT DEVELOP IM SORRY IT WONT HAPPEN AGAIN

--- Assets/Scripts/LawnmowerMover.cs
+++ Assets/Scripts/LawnmowerMover.cs
@@ -8,9 +8,9 @@ public class LawnmowerMover : MonoBehaviour
     [SerializeField] public float maxSpeed = 3.0f;
     public Rigidbody rb;
     [SerializeField] private bool isMoving = false;
-    [SerializeField] private LayerMask Wall;
+    [SerializeField] private LayerMask wall;
 
-    //[SerializeField] private AudioSource roombaStart; - podmieniæ na kosiarke
+    //[SerializeField] private AudioSource roombaStart; - podmieniï¿½ na kosiarke
 
 
     void Start()
@@ -24,7 +24,7 @@ public class LawnmowerMover : MonoBehaviour
         {
             rb.AddForce(transform.forward * speed, ForceMode.VelocityChange);
             rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
-            isMoving = !Physics.CheckSphere(transform.position, 0.1f, Wall);
+            isMoving = !Physics.CheckSphere(transform.position, 0.1f, wall);
             if (isMoving == false) { rb.velocity = Vector3.zero; }
 
         }
