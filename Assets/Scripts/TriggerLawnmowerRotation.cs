de12d215660b443c099a3fa8ad72b7f8b82e8938
Sigma <jskolska@edu.cdv.pl>
2024-04-11 18:27:30 +0200

    RotateLawnmower method added

---
+++ Assets/Scripts/TriggerLawnmowerRotation.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLawnmowerRotation : MonoBehaviour
+{
+    private bool hasTriggered = false;
+
+    private void Start()
+    {
+        Debug.Log(transform.forward, this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+
+    {
+        if (!hasTriggered)
+        {
+            LawnmowerMover lawnmowerMovement = other.GetComponent<LawnmowerMover>();
+
+            if (lawnmowerMovement != null)
+            {
+                lawnmowerMovement.RotateLawnmower(transform.forward);
+
+                hasTriggered = true;
+                //DisableCanvas();
+            }
+        }
+
+        //void DisableCanvas()
+        //{
+          //  Canvas canvas = GetComponentInChildren<Canvas>();
+
+          //  if (canvas != null)
+          //  {
+          //      canvas.enabled = false;
+           // }
+
+
+       // }
+    }
+}
