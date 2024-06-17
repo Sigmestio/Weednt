3b07866f8b214ba6bcb5c9f45d675424bc7c47b2
Sigma <jskolska@edu.cdv.pl>
2024-06-17 23:37:07 +0200

    lvls 1&2 set
    scripts and configs and etc. no new models yet

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -4,12 +4,51 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    public TMP_Text highScore1Text;
     public SceneConfigsSO sceneData;
     
     public void Level1()
     {
         SceneManager.LoadScene("Level_1");
+    }
+    
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+    
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level_3");
+    }
+    
+    public void Level4()
+    {
+        SceneManager.LoadScene("Level_4");
+    }
+    
+    public void Level5()
+    {
+        SceneManager.LoadScene("Level_5");
+    }
+    
+    public void Level6()
+    {
+        SceneManager.LoadScene("Level_6");
+    }
+   
+    public void Level7()
+    {
+        SceneManager.LoadScene("Level_7");
+    }
+    
+    public void Level8()
+    {
+        SceneManager.LoadScene("Level_8");
+    }
+    
+    public void Level9()
+    {
+        SceneManager.LoadScene("Level_9");
     }
 
     public void Menu()
@@ -31,9 +70,6 @@ public class LevelLoader : MonoBehaviour
         Debug.Log("Quit");
         Application.Quit();
     }
-    public void SetScoreText(int score)
-    {
-        highScore1Text.text = "Score: " + score;
-    }
+    
     
 }
